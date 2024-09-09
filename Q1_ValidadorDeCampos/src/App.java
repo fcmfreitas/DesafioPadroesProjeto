6982ade90a0cac18363be75c062b83b8e5bf1e06
fcmfreitas <franciscocmfreitas@gmail.com>
2024-09-09 09:36:56 -0300

    q1

--- Q1_ValidadorDeCampos/src/App.java
+++ Q1_ValidadorDeCampos/src/App.java
@@ -1,12 +1,13 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String email = "bernardo.copstein@pucrs.br";
-        var validador = new Validador();
+        Validador validador = new Validador();
 
-        if (validador.valida(Validador.Tipo.EMAIL, email)){
-            System.out.println(email+" é um email válido!");
-        }else{
-            System.out.println(email+" não é um email válido!");
+        validador.setEstrategia(new Validador.ValidadorEmail());
+        if (validador.valida(email)) {
+            System.out.println(email + " é um email válido!");
+        } else {
+            System.out.println(email + " não é um email válido!");
         }
     }
 }
--- Q1_ValidadorDeCampos/src/Validador.java
+++ Q1_ValidadorDeCampos/src/Validador.java
@@ -1,36 +1,55 @@
-public class Validador{
-    public enum Tipo { EMAIL, INTEIRO, MATRICULA }
+public class Validador {
+    private ValidadorStrategy estrategia;
 
-    public boolean valida(Tipo tipo,String valor){
-        switch(tipo){
-            case INTEIRO:
-                for(int i=0;i<valor.length();i++){
-                    if (!Character.isDigit(valor.charAt(i))){
-                        return false;
-                    }
-                }
-                return true;
-            case MATRICULA:
-                if (!valida(Tipo.INTEIRO,valor)){
+    public void setEstrategia(ValidadorStrategy estrategia) {
+        this.estrategia = estrategia;
+    }
+
+    public boolean valida(String valor) {
+        if (estrategia == null) {
+            throw new IllegalStateException("Estratégia não definida.");
+        }
+        return estrategia.valida(valor);
+    }
+
+    public interface ValidadorStrategy {
+        boolean valida(String valor);
+    }
+    
+    public static class ValidadorEmail implements ValidadorStrategy {
+        @Override
+        public boolean valida(String valor) {
+            int posA = valor.indexOf('@');
+            int posPt = valor.indexOf('.');
+            return posA > 0 && posPt > 0;
+        }
+    }
+    
+    public static class ValidadorInteiro implements ValidadorStrategy {
+        @Override
+        public boolean valida(String valor) {
+            for (int i = 0; i < valor.length(); i++) {
+                if (!Character.isDigit(valor.charAt(i))) {
                     return false;
-                }else{
-                    int sum = 0;
-                    for(int i=0;i<valor.length()-1;i++){
-                        sum += Character.getNumericValue(valor.charAt(i));
-                    }
-                    int verificador = sum%10;
-                    if (verificador == Character.getNumericValue(valor.charAt(valor.length()-1))){
-                        return true;
-                    }
                 }
+            }
+            return true;
+        }
+    }
+    
+    public static class ValidadorMatricula implements ValidadorStrategy {
+        @Override
+        public boolean valida(String valor) {
+            if (!new ValidadorInteiro().valida(valor)) {
                 return false;
-            case EMAIL:
-                int posA = valor.indexOf('@');
-                int posPt = valor.indexOf('.');
-                if (posA <= 0 || posPt <= 0) return false;
-                return true;
-            default:
-                return false;
+            } else {
+                int sum = 0;
+                for (int i = 0; i < valor.length() - 1; i++) {
+                    sum += Character.getNumericValue(valor.charAt(i));
+                }
+                int verificador = sum % 10;
+                return verificador == Character.getNumericValue(valor.charAt(valor.length() - 1));
+            }
         }
     }
 }
