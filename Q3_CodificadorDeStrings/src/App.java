be8f3159837dddaf605ed3f0786570d02047c6ff
fcmfreitas <franciscocmfreitas@gmail.com>
2024-09-09 10:24:40 -0300

    q3

--- Q3_CodificadorDeStrings/src/App.java
+++ Q3_CodificadorDeStrings/src/App.java
@@ -1,17 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Codificador cod = new Codificador();
 
+        Codificador codificadorBasico = new Codificador();
+        CodificadorComLog codificadorComLog = new CodificadorComLog(codificadorBasico);
         String aux = "Desafio Padrões de Projeto";
 
-        System.out.println(aux);
-        aux = cod.codifica(Codificador.Tipo.SIMPLES, aux);
-        System.out.println(aux);
-        aux = cod.deCodifica(Codificador.Tipo.SIMPLES, aux);
-        System.out.println(aux);
-        aux = cod.codifica(Codificador.Tipo.DESLOCA, aux);
-        System.out.println(aux);
-        aux = cod.deCodifica(Codificador.Tipo.DESLOCA, aux);
-        System.out.println(aux);
+        System.out.println("Texto original: " + aux);
+        aux = codificadorComLog.codifica(Codificador.Tipo.SIMPLES, aux);
+        System.out.println("Codificado (SIMPLES): " + aux);
+        aux = codificadorComLog.deCodifica(Codificador.Tipo.SIMPLES, aux);
+        System.out.println("Decodificado (SIMPLES): " + aux);
+        aux = codificadorComLog.codifica(Codificador.Tipo.DESLOCA, aux);
+        System.out.println("Codificado (DESLOCA): " + aux);
+        aux = codificadorComLog.deCodifica(Codificador.Tipo.DESLOCA, aux);
+        System.out.println("Decodificado (DESLOCA): " + aux);
+
+        CodificadorComLogDetalhado codificadorComLogDetalhado = new CodificadorComLogDetalhado(codificadorBasico);
+
+        aux = "Outro exemplo com log detalhado";
+        System.out.println("Texto original: " + aux);
+        
+        aux = codificadorComLogDetalhado.codifica(Codificador.Tipo.SIMPLES, aux);
+        System.out.println("Codificado (SIMPLES): " + aux);
+        
+        aux = codificadorComLogDetalhado.deCodifica(Codificador.Tipo.SIMPLES, aux);
+        System.out.println("Decodificado (SIMPLES): " + aux);
     }
 }
--- Q3_CodificadorDeStrings/src/Codificador.java
+++ Q3_CodificadorDeStrings/src/Codificador.java
@@ -1,73 +1,28 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Codificador{
-    public enum Tipo {SIMPLES,DESLOCA};
-    private Map<Character,Character> tabCod;
-    private Map<Character,Character> tabDeCod;
+public class Codificador {
+    public enum Tipo {SIMPLES, DESLOCA};
+    private Map<Character, Character> tabCod;
+    private Map<Character, Character> tabDeCod;
 
-    public Codificador(){
+    public Codificador() {
         tabCod = new HashMap<>();
-        tabCod.put('a','@');
-        tabCod.put('e','#');
-        tabCod.put('i','!');
-        tabCod.put('o','$');
-        tabCod.put('u','%');
+        tabCod.put('a', '@');
+        tabCod.put('e', '#');
+        tabCod.put('i', '!');
+        tabCod.put('o', '$');
+        tabCod.put('u', '%');
         tabDeCod = new HashMap<>();
-        tabDeCod.put('@','a');
-        tabDeCod.put('#','e');
-        tabDeCod.put('!','i');
-        tabDeCod.put('$','o');
-        tabDeCod.put('%','u');
-        
+        tabDeCod.put('@', 'a');
+        tabDeCod.put('#', 'e');
+        tabDeCod.put('!', 'i');
+        tabDeCod.put('$', 'o');
+        tabDeCod.put('%', 'u');
     }
 
-    private String codificaSimples(String str){
-        char[] aux = str.toCharArray();
-        char[] resp = new char[str.length()];
-        for(int i=0;i<aux.length;i++){
-            if (tabCod.containsKey(aux[i])){
-                resp[i] = tabCod.get(aux[i]);
-            }else{
-                resp[i] = aux[i];
-            }
-        }
-        return(new String(resp));
-    }
-
-    private String deCodificaSimples(String str){
-        char[] aux = str.toCharArray();
-        char[] resp = new char[str.length()];
-        for(int i=0;i<aux.length;i++){
-            if (tabDeCod.containsKey(aux[i])){
-                resp[i] = tabDeCod.get(aux[i]);
-            }else{
-                resp[i] = aux[i];
-            }
-        }
-        return(new String(resp));
-    }
-
-    private String codificaDesloca(String str){
-        char[] aux = str.toCharArray();
-        char[] resp = new char[str.length()];
-        for(int i=0;i<aux.length;i++){
-            resp[i] = (char)(Character.valueOf(aux[i])+1);
-        }
-        return(new String(resp));
-    }
-
-    private String deCodificaDesloca(String str){
-        char[] aux = str.toCharArray();
-        char[] resp = new char[str.length()];
-        for(int i=0;i<aux.length;i++){
-            resp[i] = (char)(Character.valueOf(aux[i])-1);
-        }
-        return(new String(resp));
-    }
-
-    public String codifica(Tipo tipo,String str){
-        switch(tipo){
+    public String codifica(Tipo tipo, String str) {
+        switch (tipo) {
             case SIMPLES:
                 return codificaSimples(str);
             case DESLOCA:
@@ -77,8 +32,8 @@ public class Codificador{
         }
     }
 
-    public String deCodifica(Tipo tipo,String str){
-        switch(tipo){
+    public String deCodifica(Tipo tipo, String str) {
+        switch (tipo) {
             case SIMPLES:
                 return deCodificaSimples(str);
             case DESLOCA:
@@ -88,4 +43,97 @@ public class Codificador{
         }
     }
 
+    private String codificaSimples(String str) {
+        char[] aux = str.toCharArray();
+        char[] resp = new char[str.length()];
+        for (int i = 0; i < aux.length; i++) {
+            if (tabCod.containsKey(aux[i])) {
+                resp[i] = tabCod.get(aux[i]);
+            } else {
+                resp[i] = aux[i];
+            }
+        }
+        return new String(resp);
+    }
+
+    private String deCodificaSimples(String str) {
+        char[] aux = str.toCharArray();
+        char[] resp = new char[str.length()];
+        for (int i = 0; i < aux.length; i++) {
+            if (tabDeCod.containsKey(aux[i])) {
+                resp[i] = tabDeCod.get(aux[i]);
+            } else {
+                resp[i] = aux[i];
+            }
+        }
+        return new String(resp);
+    }
+
+    private String codificaDesloca(String str) {
+        char[] aux = str.toCharArray();
+        char[] resp = new char[str.length()];
+        for (int i = 0; i < aux.length; i++) {
+            resp[i] = (char) (aux[i] + 1);
+        }
+        return new String(resp);
+    }
+
+    private String deCodificaDesloca(String str) {
+        char[] aux = str.toCharArray();
+        char[] resp = new char[str.length()];
+        for (int i = 0; i < aux.length; i++) {
+            resp[i] = (char) (aux[i] - 1);
+        }
+        return new String(resp);
+    }
+}
+
+interface CodificadorDecorator {
+    String codifica(Codificador.Tipo tipo, String str);
+    String deCodifica(Codificador.Tipo tipo, String str);
+}
+
+class CodificadorComLog implements CodificadorDecorator {
+    private Codificador codificador;
+
+    public CodificadorComLog(Codificador codificador) {
+        this.codificador = codificador;
+    }
+
+    @Override
+    public String codifica(Codificador.Tipo tipo, String str) {
+        String resultado = codificador.codifica(tipo, str);
+        log("Codificando", tipo, str, resultado);
+        return resultado;
+    }
+
+    @Override
+    public String deCodifica(Codificador.Tipo tipo, String str) {
+        String resultado = codificador.deCodifica(tipo, str);
+        log("Decodificando", tipo, str, resultado);
+        return resultado;
+    }
+
+    private void log(String operacao, Codificador.Tipo tipo, String entrada, String saida) {
+        System.out.println("[" + operacao + "] Tipo: " + tipo + " | Entrada: " + entrada + " | Saída: " + saida);
+    }
+}
+
+class CodificadorComLogDetalhado extends CodificadorComLog {
+
+    public CodificadorComLogDetalhado(Codificador codificador) {
+        super(codificador);
+    }
+
+    @Override
+    public String codifica(Codificador.Tipo tipo, String str) {
+        String resultado = super.codifica(tipo, str);
+        return resultado;
+    }
+
+    @Override
+    public String deCodifica(Codificador.Tipo tipo, String str) {
+        String resultado = super.deCodifica(tipo, str);
+        return resultado;
+    }
 }
